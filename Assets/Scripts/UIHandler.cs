876980b8567259d905f7b11ea3627718e97050d3
Soban Syeed <Soban.syeed@outlook.com>
2025-01-09 00:05:15 +0530

    Add UIHandler functionality

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -1,9 +1,27 @@
+using UnityEngine.SceneManagement;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class UIHandler : MonoBehaviour
 {
     public void StartNew()
     {
+        SceneManager.LoadScene(1);
+    }
 
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
