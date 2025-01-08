9d0fd10f6f6036c02c036cf67dc8d6cea6232e77
Soban Syeed <Soban.syeed@outlook.com>
2025-01-08 23:56:52 +0530

    Update UIHandler

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class UIHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void StartNew()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
